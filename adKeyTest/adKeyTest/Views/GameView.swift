8f3338ac930a58b7ac4484257fad0c73fdf19957
acidsasha <sohan.boxan@gmail.com>
2024-10-30 13:22:41 +0300

    initial commit

---
+++ adKeyTest/adKeyTest/Views/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  adKeyTest
+//
+//  Created by Alexandr Filovets on 30.10.24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        ZStack {
+            Image(systemName: "globe")
+        }
+    }
+}
+
+#Preview {
+    GameView()
+}
---
+++ adKeyTest/adKeyTest/adKeyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  adKeyTestApp.swift
+//  adKeyTest
+//
+//  Created by Alexandr Filovets on 30.10.24.
+//
+
+import SwiftUI
+
+@main
+struct adKeyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
